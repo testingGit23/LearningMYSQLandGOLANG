618929d0be5273f1d8bc86ab58eb80b1314463e8
Gligor23 <glgudjevski@gmail.com>
2018-08-07 14:25:45 +0200

    refactored new

--- visualBase/pkg/new/new.go
+++ visualBase/pkg/new/new.go
@@ -2,9 +2,17 @@ package new
 
 import (
 	"LearningMYSQLandGOLANG/visualBase/pkg/opendb"
+	"database/sql"
 	"net/http"
 )
 
-func New(w http.ResponseWriter, r *http.Request) {
-	opendb.Tmpl.ExecuteTemplate(w, "New", nil)
+func New(db *sql.DB, detailsAboutDB opendb.DbDetails, err error) func(w http.ResponseWriter, r *http.Request) {
+	if err != nil {
+		return func(w http.ResponseWriter, r *http.Request) {
+			opendb.Tmpl.ExecuteTemplate(w, "NoSuchDB", detailsAboutDB)
+		}
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		opendb.Tmpl.ExecuteTemplate(w, "New", nil)
+	}
 }
